99d386a240acfaed47d20bf0ffc1f5fc134f7f14
Belu <maria.alarcong1@gmail.com>
2023-12-10 09:54:39 -0500

    Module One Finished

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.utils.timezone import now
 
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
 
+from django.conf import settings
+import uuid
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
@@ -9,7 +16,17 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+# class CarMake(models.Model):
+# 	user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#     )
+# 	name = models.CharField(null=False, max_length=30, default='Toyota')
+# 	descripton = models.CharField(max_length=1000)
+	
+# 	def __str__(self):
+# 		return "Name: " + self.name + "," + \
+# 			"Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -20,7 +37,10 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
-
+# class CarModel(models.Model):
+# 	pass
+	# users = models.ForeignKey(CarMake, through='Enrollment')
+	# dealer_id = models.ImageField(
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
